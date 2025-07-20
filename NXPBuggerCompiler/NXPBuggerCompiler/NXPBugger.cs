40a7c8799973c8fb4a39908aba219fdd157ca9f8
hakimmc <acalgin598@gmail.com>
2025-07-20 20:42:03 +0300

    [NXPBuggerCompiler v1.0.0] added!

---
+++ NXPBuggerCompiler/NXPBuggerCompiler/NXPBugger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NXPBuggerCompiler
+{
+    public static class NXPBugger
+    {
+        public static void CreateCWA(string ConfigFileAddr, string binFileAdrr,string CWAFileName)
+        {
+            byte[] cfg_bytes;
+            cfg_bytes = File.ReadAllBytes(ConfigFileAddr);
+            File.WriteAllBytes(CWAFileName, MergeByteArrays(cfg_bytes, File.ReadAllBytes(binFileAdrr)));
+        }
+       
+        private static byte[] MergeByteArrays(byte[] array1, byte[] array2)
+        {
+            byte[] mergedArray = new byte[array1.Length + array2.Length];
+            Buffer.BlockCopy(array1, 0, mergedArray, 0, array1.Length);
+            Buffer.BlockCopy(array2, 0, mergedArray, array1.Length, array2.Length);
+            return mergedArray;
+        }
+
+    }
+}
